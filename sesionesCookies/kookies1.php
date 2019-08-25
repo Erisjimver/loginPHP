038382d3c3d7017a7bcb588f6ea9907fb0e9def9
erisjimver <erisjinver@gmail.com>
2019-08-24 23:39:10 -0500

    calculo duracion de un mes de la cookie

--- sesionesCookies/kookies1.php
+++ sesionesCookies/kookies1.php
@@ -9,7 +9,7 @@
 
 	setcookie("prueba", "Esta es la informacion de mi primer kookie",time()+30);//la duracion de una Cookie dura solo 30 segundos, es decir set time mide la exprecion en segundos
 
-
+// un mes = 60 segundos (tiene un minuto) * 60 minutos (que tiene una hora)  * 24 horas (que tiene un dia) * 30 dias (que tiene un mes)    2592000 segundos (durara un mes)
 
 	 ?>
 
