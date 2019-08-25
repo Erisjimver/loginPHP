9cf8fb88a57a221bd387c111c200e1d9d242da28
erisjimver <erisjinver@gmail.com>
2019-08-24 23:36:49 -0500

    tiempo en cookies

--- sesionesCookies/kookies1.php
+++ sesionesCookies/kookies1.php
@@ -7,7 +7,7 @@
 
 	<?php 
 
-	setcookie("prueba", "Esta es la informacion de mi primer kookie");
+	setcookie("prueba", "Esta es la informacion de mi primer kookie",time()+30);//la duracion de una Cookie dura solo 30 segundos, es decir set time mide la exprecion en segundos
 
 
 
